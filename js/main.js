87ed841276f4d1aabf7e13447e6a864e6c9e8285
Alla Selivanova <kweyjiba12@gmail.com>
2014-12-03 22:39:53 +0300

    Fix overlay.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
-		$( "#datepicker" ).datepicker({
-				showOn: "button",
-				buttonImage: "img/calendar.gif",
-				buttonImageOnly: true,
-				dateFormat: "dd.mm.yy"
+	$( "#datepicker" ).datepicker({
+		showOn: "button",
+		buttonImage: "img/calendar.gif",
+		buttonImageOnly: true,
+		dateFormat: "dd.mm.yy"
+	});
+
+	$('#photo').click( function() {
+		var src = $(this).attr('href');
+		var img = $('<img src="' + src + '">').load(function(){
+				var img = $(this);
+				img.css({marginLeft: -img.width()/2, marginTop: -img.height()/2});
 		});
-})
+
+		$('.overlay')
+			.empty()
+			.append(img)
+			.show(500)
+			.click(function(){
+				$(this).hide(500);
+			});
+
+		return false;
+	})
+});
