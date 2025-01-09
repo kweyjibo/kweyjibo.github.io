30eddd18b8c579f9781b967d388356f5cd1250fc
Alla Dolzhenko <kweyjiba12@gmail.com>
2025-01-09 11:02:13 +0100

    Blog. Fix

---
+++ src/assets/scripts/view.js
@@ -0,0 +1,24 @@
+(async function view() {
+  const el = document.getElementById("view-count");
+
+  if (el) {
+    const grabedurl = window.location.pathname;
+
+    const url = `https://counter.dolzhenko.dev/hit?url=https://kweyjibo.com${grabedurl}`;
+
+    try {
+      const response = await fetch(url, {
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const data = await response.text();
+        el.insertAdjacentText("beforeend", data);
+      } else {
+        console.error("Failed to fetch data");
+      }
+    } catch (error) {
+      console.error("❗️Error " + error.message);
+    }
+  }
+})();
